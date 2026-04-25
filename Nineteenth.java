0253b6836d8237be47d8384b0fff1c2ae4078dac
Aditya Sehrawat <adityas2124@gmail.com>
2026-04-25 18:00:24 +0530

    Add files via upload

---
+++ Nineteenth.java
@@ -0,0 +1,18 @@
+package Hard;
+import java.util.Scanner;
+public class Nineteenth {
+    public static boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n % 2 == 0) return n == 2;
+        for (long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long a = sc.nextInt();
+        sc.close();
+        System.out.println(isPrime(a) ? "Prime": "Not prime");
+    }
+}

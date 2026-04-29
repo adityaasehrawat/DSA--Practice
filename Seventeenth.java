0ad68b864194639b3da6729d6e5eef28f70570c5
Aditya Sehrawat <adityas2124@gmail.com>
2026-04-29 21:39:21 +0530

    Add files via upload

---
+++ Seventeenth.java
@@ -0,0 +1,23 @@
+package Hard;
+import java.util.Scanner;
+class Seventeenth{
+    static public void display(int total,double avg,char grade){
+        System.out.println("Total: "+total);
+        System.out.println("Average: "+avg);
+        System.out.println("Grade: "+grade);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name = sc.next();
+        int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+        int total=a+b+c;
+        double avg=total/3;
+        sc.close();
+        char grade;
+        if(avg>=75) grade = 'A';
+        else if(avg >= 60) grade = 'B';
+        else if(avg >= 50) grade = 'C';
+        else grade = 'F';
+        display(total,avg,grade);
+    }
+}

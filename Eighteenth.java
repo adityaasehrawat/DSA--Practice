6f7d479c05c38c2e59cf90f9fc1787cc8dbee39e
Aditya Sehrawat <adityas2124@gmail.com>
2026-06-01 20:17:42 +0530

    Update Eighteenth.java

--- Eighteenth.java
+++ Eighteenth.java
@@ -4,7 +4,7 @@ public class Eighteenth{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int base=sc.nextInt(),exponent=sc.nextInt();
-        System.out.println(Math.pow(base,exponent));
+        System.out.prin tln(Math.pow(base,exponent));
         sc.close();
     }
 } 

fa0469769ae2cfc094371900129312fd105d8c3c
Aditya Sehrawat <adityas2124@gmail.com>
2026-06-04 16:21:01 +0530

    Update Nineteenth.java

--- Nineteenth.java
+++ Nineteenth.java
@@ -4,7 +4,7 @@ public class Nineteenth {
     public static boolean isPrime(long n) {
         if (n < 2) return false;
         if (n % 2 == 0) return n  == 2;
-        for (long i = 3; i * i <= n; i += 2) {
+        for (long i = 3 ; i * i <= n; i += 2) {
             if (n % i == 0) return false;
         }
         return true;

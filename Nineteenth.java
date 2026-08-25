0aa41ead2f06857158f2859abc61c554a0b17c8f
Aditya Sehrawat <adityas2124@gmail.com>
2026-08-25 10:55:04 +0530

    Update Nineteenth.java

--- Nineteenth.java
+++ Nineteenth.java
@@ -5,7 +5,7 @@ public class Nineteenth {
         if (n < 2) return false;
         if (n % 2 == 0) return n  == 2;
         for (long i = 3 ; i * i <= n; i += 2) {
-            if (n % i == 0) return false;
+            if (n  % i == 0) return false;
         }
         return true;
     }
